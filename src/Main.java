09e8176de806377b93f7d1a775d554211f5f6295
Davide Tosi <dav.tosi@gmail.com>
2015-04-17 09:43:26 +0200

    WSQR S-CASE Tool
    WSQR Tool to Represent non-functional and quality aspects to Web
    Services

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+
+
+public class Main {
+
+
+	public static void main(String[] args) throws ClassNotFoundException {
+				
+		WSInterface2 window = new WSInterface2();
+		File xmlFile = new File("bb");
+	}
+	
+}
